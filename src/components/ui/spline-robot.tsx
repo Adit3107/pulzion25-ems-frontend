ca9206bf3c435139446bdde203514924e9fe620e
Aditya Kalmegh <adityakalmegh01@gmail.com>
2025-09-15 17:50:22 +0000

    https://prod.spline.design/UH53PlFYPAxwAc5N/scene.splinecode <script typ

--- src/components/ui/spline-robot.tsx
+++ src/components/ui/spline-robot.tsx
@@ -5,7 +5,7 @@ import Spline from '@splinetool/react-spline';
 export function SplineRobot() {
   return (
     <div className="w-full h-full">
-      <Spline scene="https://prod.spline.design/xM8YkXXoBImRVEzQ/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/UH53PlFYPAxwAc5N/scene.splinecode" />
     </div>
   );
 }
