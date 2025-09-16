4f33b73ca6bc94f125f1a156e09cd693a0aa0cb9
Aditya Kalmegh <adityakalmegh01@gmail.com>
2025-09-16 09:26:54 +0000

    Try fixing this error: `Runtime Error: Error: Data read, but end of buff

--- src/components/ui/wireframe-cube.tsx
+++ src/components/ui/wireframe-cube.tsx
@@ -5,7 +5,7 @@ import Spline from '@splinetool/react-spline';
 export function WireframeCube() {
   return (
     <div className="w-full h-full aspect-square">
-      <Spline scene="https://prod.spline.design/i-289gTQrKx3mB2n/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/B8sA-2i2G4a47l55/scene.splinecode" />
     </div>
   );
 }
